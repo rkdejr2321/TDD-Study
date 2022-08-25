75441ff75f41a8a055b71cf06f9c9b123502e634
Seo Kang Deok <rkdejr2321@naver.com>
2022-08-25 22:13:25 +0900

    [FATCH] chap 07

--- src/test/java/com/example/TDD/chap07/user/UserRegisterTest.java
+++ src/test/java/com/example/TDD/chap07/user/UserRegisterTest.java
@@ -1,5 +1,6 @@
 package com.example.TDD.chap07.user;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
