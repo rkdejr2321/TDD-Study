2d02c2734e8870ff86eca38e07f707eae2a3b8e6
Seo Kang Deok <rkdejr2321@naver.com>
2022-08-25 22:11:55 +0900

    [FATCH] chap07 완료된 코드 main에 넣음

--- src/main/java/com/example/TDD/chap07/autodebit/AutoDebitRegisterTest.java
+++ src/main/java/com/example/TDD/chap07/autodebit/AutoDebitRegisterTest.java
@@ -23,7 +23,7 @@ public class AutoDebitRegisterTest {
         // 업체에서 받은 테스트용 유효한 카드 번호 사용
         AutoDebitReq req = new AutoDebitReq("user1", "1234123412341234");
         RegisterResult result = this.register.register(req);
-        assertEquals(VALID, result.getValidity());
+        Assertions.assertEquals(VALID, result.getValidity());
     }
 
     @Test
@@ -31,6 +31,6 @@ public class AutoDebitRegisterTest {
         // 업체에서 받은 도난 테스트용 카드 번호 사용
         AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
         RegisterResult result = this.register.register(req);
-        assertEquals(THEFT, result.getValidity());
+        Assertions.assertEquals(THEFT, result.getValidity());
     }
 }
---
+++ src/test/java/com/example/TDD/appendixC/UserRegisterMockTest.java
@@ -0,0 +1,46 @@
+package com.example.TDD.appendixC;
+
+import com.example.TDD.chap07.user.EmailNotifier;
+import com.example.TDD.chap07.user.MemoryUserRepository;
+import com.example.TDD.chap07.user.StubWeakPasswordChecker;
+import com.example.TDD.chap07.user.UserRegister;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRegisterMockTest {
+    private UserRegister userRegister;
+    private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    @Mock
+    private EmailNotifier mockEmailNotifier;
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(stubPasswordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendEmail() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        then(mockEmailNotifier).should().sendRegisterEmail(captor.capture());
+
+        String realEmail = captor.getValue();
+        Assertions.assertEquals("email@email.com", realEmail);
+    }
+
+}
